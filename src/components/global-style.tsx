ed6779561f5fdbea6836081f641389ece9c61532
Mathias Søholm <mss@trifork.com>
2020-03-24 17:58:47 +0100

    Add custom font for title

--- src/components/global-style.tsx
+++ src/components/global-style.tsx
@@ -5,6 +5,15 @@ const GlobalStyle: React.FC = () => {
   return (
     <Global
       styles={css`
+        @font-face {
+          font-family: 'Dancing Script';
+          font-style: normal;
+          font-weight: 400;
+          src: local(''),
+            url('/fonts/dancing-script-v14-latin-regular.woff2') format('woff2'),
+            url('/fonts/dancing-script-v14-latin-regular.woff') format('woff');
+        }
+
         html {
           --html-background: #dcd7d3;
           --content-background: #ede4df;
@@ -14,6 +23,11 @@ const GlobalStyle: React.FC = () => {
 
         button {
           cursor: pointer;
+        }
+
+        a {
+          text-decoration: none;
+          color: inherit;
         }
       `}
     />
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -32,15 +32,18 @@ const Layout: React.FC = ({ children }) => {
           margin: auto;
           min-height: 100vh;
           padding: 16px 50px;
+          box-shadow: 0px 4px 70px rgba(0, 0, 0, 0.15);
         `}
       >
-        <h3
+        <h1
           css={css`
             margin-top: 0;
+            font-family: 'Dancing Script';
+            font-size: 30px;
           `}
         >
           <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
-        </h3>
+        </h1>
         {children}
       </div>
     </>
