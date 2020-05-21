f278008529be255cfdfb03d37cdafac7b5d4cdb2
Mathias Søholm <mathiassoeholm@gmail.com>
2020-05-21 13:34:00 +0200

    Use faker to generate test data

--- cypress/e2e/contact-page.ts
+++ cypress/e2e/contact-page.ts
@@ -1,11 +1,13 @@
+import faker from "faker";
+
 describe("contact-page", () => {
   it("can submit", () => {
     cy.visit("/");
     cy.findByTestId("nav-link-to-/contact").click();
-    cy.get("#name").type("Bob");
-    cy.get("#email").type("bob@mail.com");
-    cy.get("#subject").type("Cypress test");
-    cy.get("#message").type("This is a message from Cypress");
+    cy.get("#name").type(faker.name.findName());
+    cy.get("#email").type(faker.internet.email());
+    cy.get("#subject").type(faker.lorem.words());
+    cy.get("#message").type(faker.lorem.paragraphs());
     cy.get('[type="submit"]').click();
 
     cy.findByTestId("thank-you-text");
