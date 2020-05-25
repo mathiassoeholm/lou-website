3028bf281c2b702a4c4cd1ebe324efe24c7ebd20
Mathias Soeholm <mathiassoeholm@gmail.com>
2020-05-25 21:50:20 +0200

    Start work on testing error condition

--- cypress/e2e/contact-page.ts
+++ cypress/e2e/contact-page.ts
@@ -1,15 +1,44 @@
 import faker from "faker";
 
 describe("contact-page", () => {
+  function getFakeEntry() {
+    return {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      subject: faker.lorem.words(),
+      message: faker.lorem.words(),
+    };
+  }
+
   it("can submit", () => {
     cy.visit("/");
     cy.findByTestId("nav-link-to-/contact").click();
-    cy.get("#name").type(faker.name.findName());
-    cy.get("#email").type(faker.internet.email());
-    cy.get("#subject").type(faker.lorem.words());
-    cy.get("#message").type(faker.lorem.paragraphs());
+
+    const fakeEntry = getFakeEntry();
+
+    cy.get("#name").type(fakeEntry.name);
+    cy.get("#email").type(fakeEntry.email);
+    cy.get("#subject").type(fakeEntry.subject);
+    cy.get("#message").type(fakeEntry.message);
     cy.get('[type="submit"]').click();
 
     cy.findByTestId("thank-you-text");
+  });
+
+  it("should show an error message if the request fails", () => {
+    cy.server();
+    cy.route(`${Cypress.config().baseUrl}.netlify/funtions/contact-submit`, {});
+
+    const fakeEntry = getFakeEntry();
+
+    cy.visit("/contact");
+    cy.findByTestId("nav-link-to-/contact").click();
+    cy.get("#name").type(fakeEntry.name);
+    cy.get("#email").type(fakeEntry.email);
+    cy.get("#subject").type(fakeEntry.subject);
+    cy.get("#message").type(fakeEntry.message);
+    cy.get('[type="submit"]').click();
+
+    cy.findByTestId("error-text");
   });
 });
